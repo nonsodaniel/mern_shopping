f11b6673d427679d7b1c9c6504568127b69e2bf5
nonsodaniel <nonsodaniel07@gmail.com>
2019-12-04 22:12:56 +0100

    So many changes made and moving to a newer system

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+// import './bootstrap.min.css'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Hello <button className="btn btn-primary">hello</button></h1>
+    </div>
+  );
+}
+
+export default App;
