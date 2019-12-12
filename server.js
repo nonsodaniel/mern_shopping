ba1dddad1b8c2152d838e884dabb415b2b46b022
nonsodaniel <nonsodaniel07@gmail.com>
2019-12-12 14:01:16 +0100

    Added heroku routing in server

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-const items = require('./routes/api/Items')
+const items = require("./routes/api/Items");
+const path = require("path");
 
 const app = express();
 
@@ -10,14 +11,27 @@ app.use(bodyParser.urlencoded({ extended: false, limit: "50mb" }));
 app.use(bodyParser.json());
 
 //db config
-const Db = require("./config/keys").mongoURI
+const Db = require("./config/keys").mongoURI;
 
 //connect to mongo
-mongoose.connect(Db, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => console.log("DB connected")).catch(err => console.log(err))
+mongoose
+  .connect(Db, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("DB connected"))
+  .catch(err => console.log(err));
 
 //use ROutes
-app.use('/api/items', items)
+app.use("/api/items", items);
+
+// on production
+if (process.env.NODE_ENV === "production") {
+  //set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const port = process.env.PORT || 4000;
 
-app.listen(port, () => console.log("Server started on port", port))
+app.listen(port, () => console.log("Server started on port", port));
